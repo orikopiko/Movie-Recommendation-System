b6c1b23b69c399cd52fdae1fc345a921eaa5334b
orikopiko <59078410+orikopiko@users.noreply.github.com>
2023-06-07 18:44:43 -0700

    test 3

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df = pd.read_csv('datasets/ratings.csv')
-print(df)
+df_ratings = pd.read_csv('datasets/ratings.csv')
+print(df_ratings)
+df_movies = pd.read_csv('datasets/movies.csv')
+print(df_movies)
+df_tags = pd.read_csv('datasets/tags.csv')
+print(df_tags)
+
+df_tags['tag'] = df_tags['tag'].str.lower()
+print(df_tags)
